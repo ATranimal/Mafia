3a4532407c2d79d4805a44e4d79fca8d756b8901
Andrew Tran <tran.andrew.a@gmail.com>
2017-08-28 17:03:06 -0400

    Added fuctional buttons on the index
    Added the relevant django model to javascript code, and button
    functionality for displaying setup information on the index

--- mysite/mafia/static/mafia/js/index.js
+++ mysite/mafia/static/mafia/js/index.js
@@ -1,3 +1,15 @@
-var testVariable = 
+console.log(setupList);
+console.log(setupList.length);
+console.log(setupList[0].fields.name);      
 
-console.log(testVariable);
+$("button").click(function() { 
+    $(".setup_info").empty();
+
+    for (var i = 0; i < setupList.length; i++) {
+        if (setupList[i].fields.name == $(this).attr('id'))
+        {
+            $(".setup_info").append("<p>Name: " + setupList[i].fields.name + "</p>" +
+                                    "<p>Description: " + setupList[i].fields.description + "</p>");    
+        }
+    }
+});
--- mysite/mafia/views.py
+++ mysite/mafia/views.py
@@ -2,8 +2,7 @@ from django.http import HttpResponse
 from django.template import loader
 
 from django.views import generic
-
-from django.shortcuts import render, get_object_or_404
+from django.core import serializers
 
 from .models import Setup, Role
 
@@ -12,33 +11,21 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_setup_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
         return Setup.objects.order_by('-name')[:5] 
 
-    # latest_setup_list = Setup.objects.order_by('-playerNumber')[:5] 
-    # context = {
-    #     'latest_setup_list': latest_setup_list,
-    # }
-    # return render(request, 'mafia/index.html', context)
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+
+        context["setups"] = serializers.serialize("json", Setup.objects.all());
+        return context
 
 class SetupDetailView(generic.DetailView):
     model = Setup
     template_name = 'mafia/setup_detail.html'
 
-    # setup = get_object_or_404(Setup, pk=setup_id)
-    # roleList = setup.roles.all()
-    # context = {
-    #     'setup': setup,
-    #     'roleList': roleList
-    # }
-    # return render(request, 'mafia/setup_detail.html', context)
-
 class RoleDetailView(generic.DetailView):
     model = Role
     template_name = 'mafia/role_detail.html'    
-    # role = get_object_or_404(Role, pk=role_id)
-    # setups = role.setup.all()
-    # return render(request, 'mafia/role_detail.html', {'role': role,'setups': setups})
 
 class HowToPlayView(generic.TemplateView):
     template_name = 'mafia/howtoplay.html'   
