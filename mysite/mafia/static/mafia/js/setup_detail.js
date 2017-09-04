cca9f07640eb7fd0cffb8827a22c0bd2b9f7b518
ATranimal <tran.andrew.a@gmail.com>
2017-09-04 02:57:46 -0400

    Changed colours
    Some of the colours messed up on different monitors.

--- mysite/mafia/static/mafia/js/setup_detail.js
+++ mysite/mafia/static/mafia/js/setup_detail.js
@@ -8,38 +8,40 @@ var intialPlayerNumber = $(".players").val();
 for (var i = 0; i < intialPlayerNumber; i++) {
 	if(alignments[i] == "Mafia") {
 		$(".role_list").append("<li><a href=# class="+ alignments[i] +" id='"+ setup[i] +"'>"+ setup[i] +"</a>"
-                        );	
+            );	
 	}
 }
 for (var i = 0; i < intialPlayerNumber; i++) {
 	if(alignments[i] == "Town") {
 		$(".role_list").append("<li><a href=# class="+ alignments[i] +" id='"+ setup[i] +"'>"+ setup[i] +"</a>"
-						);
+          );
 	}
 }
 
 // Code for showing role info
-$("a").click(function() {
+function bindClick() {
+    $("a").click(function() {
+        $(".panel-heading").empty();
+        $(".panel-body").empty();
+        $(".panel").fadeIn(200);
+        console.log($(this).attr('id'));
 
-    $(".panel-heading").empty();
-    $(".panel-body").empty();
-    $(".panel").fadeIn(200);
-    console.log($(this).attr('id'));
-
-    for (var i = 0; i < roleList.length; i++) {
-        if (roleList[i].fields.name == $(this).attr('id'))
-        {
-            $(".panel-heading").append(roleList[i].fields.name);
-            $(".panel-body").append("<p>" + roleList[i].fields.description + "</p>");   
-            $("panel-body").append("POOP");
-            if(roleList[i].fields.action != "n/a") {
-            	$(".panel-body").append("<p>" + roleList[i].fields.action + "</p>");
-            } 
-            $("panel-body").append("<a href=roles/" + roleList[i].pk + "><p>Click here to see more details!</p></a>");
-            $("panel-body").append("POOP");
+        for (var i = 0; i < roleList.length; i++) {
+            if (roleList[i].fields.name == $(this).attr('id'))
+            {
+                $(".panel-heading").append(roleList[i].fields.name);
+                $(".panel-body").append("<p>" + roleList[i].fields.description + "</p>");   
+                $("panel-body").append("POOP");
+                if(roleList[i].fields.action != "n/a") {
+                    $(".panel-body").append("<p>" + roleList[i].fields.action + "</p>");
+                } 
+                $("panel-body").append("<a href=roles/" + roleList[i].pk + "><p>Click here to see more details!</p></a>");
+                $("panel-body").append("POOP");
+            }
         }
-    }
-});
+    });
+}
+
 
 // Recreating the role list based on slider
 $(".players").change(function() {
@@ -53,35 +55,36 @@ $(".players").change(function() {
     for (var i = 0; i < playerNumber; i++) {
         if(alignments[i] == "Mafia") {
             $(".role_list").append("<li><a href=# class="+ alignments[i] +" id='"+ setup[i] +"''>"+ setup[i] +"</a>"
-                                    );  
+                );  
         }
     }
     for (var i = 0; i < playerNumber; i++) {
         if(alignments[i] == "Town") {
             $(".role_list").append("<li><a href=# class="+ alignments[i] +" id='"+ setup[i] +"''>"+ setup[i] +"</a>"
-                            );
+                );
         }
     }
 
     $("a").click(function() {
 
-    $(".panel-heading").empty();
-    $(".panel-body").empty();
-    $(".panel").fadeIn(200);
-    console.log($(this).attr('id'));
+        $(".panel-heading").empty();
+        $(".panel-body").empty();
+        $(".panel").fadeIn(200);
+        console.log($(this).attr('id'));
 
-    for (var i = 0; i < roleList.length; i++) {
-        if (roleList[i].fields.name == $(this).attr('id'))
-        {
-            $(".panel-heading").append(roleList[i].fields.name);
-            $(".panel-body").append("<p>" + roleList[i].fields.description + "</p>");   
-            $("panel-body").append("POOP");
-            if(roleList[i].fields.action != "n/a") {
-                $(".panel-body").append("<p>" + roleList[i].fields.action + "</p>");
-            } 
-            $("panel-body").append("<a href=roles/" + roleList[i].pk + "><p>Click here to see more details!</p></a>");
-            $("panel-body").append("POOP");
+        for (var i = 0; i < roleList.length; i++) {
+            if (roleList[i].fields.name == $(this).attr('id'))
+            {
+                $(".panel-heading").append(roleList[i].fields.name);
+                $(".panel-body").append("<p>" + roleList[i].fields.description + "</p>");   
+                $("panel-body").append("POOP");
+                if(roleList[i].fields.action != "n/a") {
+                    $(".panel-body").append("<p>" + roleList[i].fields.action + "</p>");
+                } 
+                console.log("nothing happens");
+                $("panel-body").append("<a href=roles/" + roleList[i].pk + "><p>Click here to see more details!</p></a>");
+                $("panel-body").append("POOP");
+            }
         }
-    }
-});
+    });
 });
